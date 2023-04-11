ae92df5131a8d9c089587444d3657bc5230a59a8
Reef Turner <feerrenrut@users.noreply.github.com>
2023-04-11 18:59:24 +0800

    Handle other no auto shift for other mod keys
    Note, however, the other layers that need auto-shift (symbols, numbers, numpad) also need a no shift version.
    For this reason, it would be helpful to add a new behavior for this

--- docker_build.sh
+++ docker_build.sh
@@ -3,4 +3,9 @@
 # Ensure this directory is mapped as the ZMK-config volume.
 # Change directory to /workspace/zmk-config/
 # run: `source docker_build.sh`
-west build -s "/workspaces/zmk/app/" -b slicemk_ergodox_202207_green_left  --build-dir "/workspaces/zmk-config/build" -- -DSHIELD=slicemk_ergodox_leftcentral -DZMK_CONFIG="/workspaces/zmk-config/config" 
+
+# build left-central. Works
+west build --pristine -s "/workspaces/zmk/app/" -b slicemk_ergodox_202207_green_left  --build-dir "/workspaces/zmk-config/left-central" -- -DSHIELD=slicemk_ergodox_leftcentral -DZMK_CONFIG="/workspaces/zmk-config/config"
+
+# build dongle-central. Works
+west build --pristine -s "/workspaces/zmk/app/" -b raytac_mdbt50q_rx_green  --build-dir "/workspaces/zmk-config/dongle" -- -DSHIELD=slicemk_ergodox_dongle -DZMK_CONFIG="/workspaces/zmk-config/config"
